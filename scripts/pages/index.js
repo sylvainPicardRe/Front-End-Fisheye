35f9dcedc166c72bf4319b9da9e8167890bcccff
sylvainPicardRe <57808412+sylvainPicardRe@users.noreply.github.com>
2024-10-07 08:54:40 +0200

    Ajout des images + Récupération des datas des photographes

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,17 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        const reponse = await fetch("./data/photographers.json");
+        const result = await reponse.json();
+        const photographersDatas = result.photographers;
+        let photographers = [];
+        for(let i = 0; i < photographersDatas.length; i++){
+            photographers.push(photographersDatas[i]);
+        }
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers
+        })
     }
 
     async function displayData(photographers) {
