529dd2a6f30fc2be999f3e7345f7639118cd0d07
sylvainPicardRe <57808412+sylvainPicardRe@users.noreply.github.com>
2024-10-10 08:30:43 +0200

    Ajout du js et du css pour que l'image et la balise h2 soit clicable

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,26 +1,37 @@
 function photographerTemplate(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { id, name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
+
+    const photographerLink = `photographer.html?id=${id}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture);
         img.setAttribute("alt", "image de profile");
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const a = document.createElement( 'a' );
+        a.setAttribute("href", photographerLink);
+        a.appendChild(img);
+        a.appendChild(h2);
+
         const h3 = document.createElement( 'h3' );
         h3.setAttribute("class", "margin-small font-size-small");
         h3.textContent = city + ", " + country;
+
         const description = document.createElement( 'p' );
         description.setAttribute("class", "margin-small font-size-small");
         description.textContent = tagline;
+
         const pricing = document.createElement( 'p' );
         pricing.setAttribute("class", "margin-small font-size-small princing");
         pricing.textContent = price + "€/jour";
-        article.appendChild(img);
-        article.appendChild(h2);
+        
+        article.appendChild(a);
         article.appendChild(h3);
         article.appendChild(description);
         article.appendChild(pricing);
