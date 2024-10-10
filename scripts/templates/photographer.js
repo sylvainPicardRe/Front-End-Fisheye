f96fe5d0a4a86e979d1c7fffe949da5abd43e305
sylvainPicardRe <57808412+sylvainPicardRe@users.noreply.github.com>
2024-10-10 07:54:15 +0200

    Ajout du html et du css pour la page d'accueil

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,29 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "image de profile");
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement( 'h3' );
+        h3.setAttribute("class", "margin-small font-size-small");
+        h3.textContent = city + ", " + country;
+        const description = document.createElement( 'p' );
+        description.setAttribute("class", "margin-small font-size-small");
+        description.textContent = tagline;
+        const pricing = document.createElement( 'p' );
+        pricing.setAttribute("class", "margin-small font-size-small princing");
+        pricing.textContent = price + "€/jour";
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(description);
+        article.appendChild(pricing);
         return (article);
     }
     return { name, picture, getUserCardDOM }
